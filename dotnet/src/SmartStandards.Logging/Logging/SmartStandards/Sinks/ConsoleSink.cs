4c47303c3152f26e8739a613e4df8975700825b7
T. Korn <tobias.korn@towerswatson.com>
2025-05-27 17:18:25 +0200

    small method-split in console-sink (to get one part reusabale)

--- dotnet/src/SmartStandards.Logging/Logging/SmartStandards/Sinks/ConsoleSink.cs
+++ dotnet/src/SmartStandards.Logging/Logging/SmartStandards/Sinks/ConsoleSink.cs
@@ -11,50 +11,55 @@ namespace Logging.SmartStandards.Sinks {
       int kindId, string messageTemplate, object[] args
     ) {
 
-      var rescuedColor = Console.ForegroundColor;
-
       StringBuilder logParaphBuilder = new StringBuilder(messageTemplate.Length + 20);
 
       LogParaphRenderer.BuildParaphResolved(
         logParaphBuilder, audienceToken, level, sourceContext, sourceLineId, kindId, messageTemplate, args
       );
 
+      WriteToConsole(level, logParaphBuilder);
+    }
+
+    internal static void WriteToConsole(int level, StringBuilder logParaphBuilder) {
+
+      var rescuedColor = Console.ForegroundColor;
+
       switch (level) {
 
         case 5: { // Critical => StdErr
-          Console.ForegroundColor = ConsoleColor.Magenta;
-          Console.Error.WriteLine(logParaphBuilder.ToString());
-          break;
-        }
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Error.WriteLine(logParaphBuilder.ToString());
+            break;
+          }
 
         case 4: { // Error => StdErr
-          Console.ForegroundColor = ConsoleColor.Red;
-          Console.Error.WriteLine(logParaphBuilder.ToString());
-          break;
-        }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine(logParaphBuilder.ToString());
+            break;
+          }
 
         case 3: { // Warning => StdOut
-          Console.ForegroundColor = ConsoleColor.DarkYellow;
-          Console.WriteLine(logParaphBuilder.ToString()); // Warnings go through StdOut, because they usually don't abort higher level actions
-          break;
-        }
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(logParaphBuilder.ToString()); // Warnings go through StdOut, because they usually don't abort higher level actions
+            break;
+          }
 
         case 2: { // Information => StdOut
-          Console.WriteLine(logParaphBuilder.ToString());
-          break;
-        }
+            Console.WriteLine(logParaphBuilder.ToString());
+            break;
+          }
 
         case 1: { // Debug => StdOut
-          Console.ForegroundColor = ConsoleColor.DarkCyan;
-          Console.WriteLine(logParaphBuilder.ToString());
-          break;
-        }
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(logParaphBuilder.ToString());
+            break;
+          }
 
         default: { // Trace => StdOut
-          Console.ForegroundColor = ConsoleColor.DarkGray;
-          Console.WriteLine(logParaphBuilder.ToString());
-          break;
-        }
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(logParaphBuilder.ToString());
+            break;
+          }
 
       }
 
